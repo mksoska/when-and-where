b56db632a2130671c18926fcc05f80f574fc6cb3
Marek Kadlečík <xkadlec5@fi.muni.cz>
2022-10-01 18:20:27 +0200

    Defined DbSets and added sqlite package

--- WhenAndWhereDAL/Data/WhenAndWhereDBContext.cs
+++ WhenAndWhereDAL/Data/WhenAndWhereDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PizzaShopDAL.Models;
 
 namespace PizzaShopDAL.Data;
 
@@ -6,15 +7,21 @@ public class WhenAndWhereDBContext : DbContext
 {
     private const string DatabaseName = "When&Where";
 
-    private const string ConnectionString =
+    private const string ConnectionString =  // TODO change string
         $"Server=(localdb)\\mssqllocaldb;Integrated Security=True;MultipleActiveResultSets=True;Database={DatabaseName};Trusted_Connection=True;";
 
-    // TODO define DbSets
+    public DbSet<Address> Address { get; set; }
+    public DbSet<Admin> Admin { get; set; }
+    public DbSet<Meetup> Meetup { get; set; }
+    public DbSet<Option> Option { get; set; }
+    public DbSet<User> User { get; set; }
+    public DbSet<UserMeetup> UserMeetup { get; set; }
+    public DbSet<UserOption> UserOption { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder
-            .UseSqlServer(ConnectionString)
+            .UseSqlite(ConnectionString)
             .UseLazyLoadingProxies();
     }
 
