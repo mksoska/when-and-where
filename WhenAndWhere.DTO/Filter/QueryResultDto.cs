4dac947cfb471cd2f37cc585a1bb047038e17c30
Marek Kadlečík <xkadlec5@fi.muni.cz>
2022-11-16 17:46:40 +0100

    Implemented UserQueryObject

---
+++ WhenAndWhere.DTO/Filter/QueryResultDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WhenAndWhere.DTO.Filter;
+
+public class QueryResultDto<TDto>
+{
+    public long TotalItemsCount { get; set; }
+    public int? RequestedPageNumber { get; set; }
+    public int PageSize { get; set; }
+    public IEnumerable<TDto> Items { get; set; }
+}
