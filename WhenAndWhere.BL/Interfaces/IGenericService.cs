5d63a9b8f3b2bc3abaebff8254c060b3ff4f3943
Marek Kadlečík <xkadlec5@fi.muni.cz>
2022-11-17 01:26:33 +0100

    Created GenericService

---
+++ WhenAndWhere.BL/Interfaces/IGenericService.cs
@@ -0,0 +1,14 @@
+﻿namespace WhenAndWhere.BL.Interfaces;
+
+public interface IGenericService<TDto> where TDto : class
+{
+    Task<List<TDto>> GetAll();
+
+    Task<TDto> GetById(int id);
+
+    void Create(TDto addressDto);
+
+    void Update(TDto addressDto);
+    
+    void Delete(int id);
+}
---
+++ WhenAndWhere.BL/Services/GenericService.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using WhenAndWhere.BL.Interfaces;
+using WhenAndWhere.Infrastructure.Repository;
+
+namespace WhenAndWhere.BL.Services;
+
+public class GenericService<TDto, TEntity> : IGenericService<TDto> where TDto : class where TEntity : class
+{
+    private readonly IMapper _mapper;
+    private readonly IRepository<TEntity> _repository;
+
+    public GenericService(IRepository<TEntity> repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+    
+    public async Task<List<TDto>> GetAll()
+    {
+        var addresses = await _repository.GetAll();
+        return _mapper.Map<List<TDto>>(addresses);
+    }
+
+    public async Task<TDto> GetById(int id)
+    {
+        var address = await _repository.GetById(id);
+        return _mapper.Map<TDto>(address);
+    }
+
+    public void Create(TDto addressDto)
+    {
+        var address = _mapper.Map<TEntity>(addressDto);
+        _repository.Insert(address);
+    }
+
+    public void Update(TDto addressDto)
+    {
+        var address = _mapper.Map<TEntity>(addressDto);
+        _repository.Update(address);
+    }
+
+    public void Delete(int id)
+    {
+        _repository.Delete(id);
+    }
+}
