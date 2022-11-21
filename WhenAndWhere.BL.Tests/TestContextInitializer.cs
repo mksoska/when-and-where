9acb0e63e4db039bcad4ee9fc0a2cda62ef9419c
Marek Kadlečík <xkadlec5@fi.muni.cz>
2022-11-21 01:13:57 +0100

    Fixed reflexion in QueryObjectGeneric, added QueryObject tests

---
+++ WhenAndWhere.BL.Tests/TestContextInitializer.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using WhenAndWhere.DAL;
+using WhenAndWhere.DAL.Models;
+using WhenAndWhere.Infrastructure.EFCore;
+
+namespace WhenAndWhere.BL.Tests;
+
+public class TestContextInitializer
+{
+    protected WhenAndWhereDBContext dbContext;
+    protected IMapper mapper = new Mapper(new MapperConfiguration(cfg => { cfg.AddProfile(new EFCoreProfile()); }));
+
+    public TestContextInitializer()
+    {
+        var myDatabaseName = "mydatabase_" + DateTime.Now.ToFileTimeUtc();
+
+        var dbContextOptions = new DbContextOptionsBuilder<WhenAndWhereDBContext>()
+            .UseInMemoryDatabase(databaseName: myDatabaseName)
+            .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+            .UseLazyLoadingProxies()
+            .Options;
+
+        dbContext = new WhenAndWhereDBContext(dbContextOptions);
+
+        var user1 = new User
+        {
+            Id = 1,
+            Name = "Jozef",
+            Surname = "Bohdan",
+            Email = "and@hob.it",
+            Avatar = new byte[] {0xAA, 0xAA, 0xAA},
+            PhoneNumber = "0011223344"
+        };
+
+        var user2 = new User
+        {
+            Id = 2,
+            Name = "Jozef",
+            Surname = "Patrovic",
+            Email = "cerstvy@vzduch.chyba",
+            Avatar = new byte[] {0xAA, 0xAA, 0xAA},
+            PhoneNumber = "0011223355"
+        };
+        var user3 = new User
+        {
+            Id = 3,
+            Name = "Eugen",
+            Surname = "Patrovic",
+            Email = "aj@repujem.trosku",
+            Avatar = new byte[] {0xAA, 0xAA, 0xAA},
+            PhoneNumber = "0011223366"
+        };
+
+        dbContext.User.Add(user1);
+        dbContext.User.Add(user2);
+        dbContext.User.Add(user3);
+
+        dbContext.SaveChanges();
+    }
+}
--- WhenAndWhere.BL/Query/QueryObjectGeneric.cs
+++ WhenAndWhere.BL/Query/QueryObjectGeneric.cs
@@ -1,4 +1,6 @@
-﻿using AutoMapper;
+﻿using System.Linq.Expressions;
+using AutoMapper;
+using Castle.Core.Internal;
 using WhenAndWhere.DTO.Filter;
 using WhenAndWhere.Infrastructure.Query;
 
@@ -20,12 +22,23 @@ public class QueryObjectGeneric<TEntityDto, TEntity> where TEntity : class, new(
         var query = myQuery;
 
         foreach (var col in filter.WhereColumns) {
-            var propType = filter.GetType().GetProperty(col).GetType();
-            var propValue = filter.GetType().GetProperty(col).GetValue(filter);
+            var propType = filter.Values.GetType().GetProperty(col).PropertyType;
+            var propValue = filter.Values.GetType().GetProperty(col).GetValue(filter.Values);
+
+            ParameterExpression paramExpr = Expression.Parameter(propType, "obj");
+            
+            LambdaExpression lambdaExpr = Expression.Lambda(
+                Expression.Equal(
+                    paramExpr,
+                    Expression.Constant(propValue)
+                ),
+                new List<ParameterExpression>() { paramExpr }
+            );
+
             Func<object, bool> predicate = obj => obj == col;
             query = (IQuery<TEntity>?) query.GetType().GetMethod("Where")
                 .MakeGenericMethod(propType)
-                .Invoke(query, new object[] {predicate, propValue});
+                .Invoke(query, new object[] {lambdaExpr, col});
         }
 
         if (!string.IsNullOrWhiteSpace(filter.SortCriteria)) {
@@ -36,7 +49,9 @@ public class QueryObjectGeneric<TEntityDto, TEntity> where TEntity : class, new(
             query = query.Page(filter.RequestedPageNumber.Value, filter.PageSize);
         }
 
-        query = query.Select(filter.SelectColumns);
+        if (!filter.SelectColumns.IsNullOrEmpty()) {
+            query = query.Select(filter.SelectColumns);    
+        }
 
         return mapper.Map<QueryResultDto<TEntityDto>>(query.Execute());
     }
