d9a0c9c12caecf44a6a5850cf334c034a76251ef
Marek Kadlečík <xkadlec5@fi.muni.cz>
2022-11-23 01:16:24 +0100

    Added QueryObject order test

--- WhenAndWhere.BL.Tests/QueryObjectTests.cs
+++ WhenAndWhere.BL.Tests/QueryObjectTests.cs
@@ -38,4 +38,65 @@ public class QueryObjectTests : TestContextInitializer
 
         actual.Items.Should().HaveCount(2);
     }
+
+    [Fact]
+    public void UserNameOrderTest()
+    {
+        var userDto = new UserDTO
+        {
+            Id = 3,
+            Name = "Jozef",
+            Surname = "Bohdan",
+            Email = "and@hob.it",
+            Avatar = new byte[] {0xAA, 0xAA, 0xAA},
+            PhoneNumber = "0011223344"
+        };
+
+        var queryFilterDto = new QueryFilterDto<UserDTO>
+        {
+            Values = userDto,
+            WhereColumns = new List<string>(),
+            RequestedPageNumber = 0,
+            PageSize = 10,
+            SortCriteria = "Name",
+            SortAscending = true,
+            SelectColumns = null
+        };
+
+        var expected = new List<UserDTO>
+        {
+            new()
+            {
+                Id = 3,
+                Name = "Eugen",
+                Surname = "Patrovic",
+                Email = "aj@repujem.trosku",
+                Avatar = new byte[] {0xAA, 0xAA, 0xAA},
+                PhoneNumber = "0011223366"
+            },
+            new()
+            {
+                Id = 1,
+                Name = "Jozef",
+                Surname = "Bohdan",
+                Email = "and@hob.it",
+                Avatar = new byte[] {0xAA, 0xAA, 0xAA},
+                PhoneNumber = "0011223344"
+            },
+            new()
+            {
+                Id = 2,
+                Name = "Jozef",
+                Surname = "Patrovic",
+                Email = "cerstvy@vzduch.chyba",
+                Avatar = new byte[] {0xAA, 0xAA, 0xAA},
+                PhoneNumber = "0011223355"
+            }
+        };
+
+        var queryObject = new UserQueryObject(mapper, new EntityFrameworkQuery<User>(dbContext));
+        var actual = queryObject.ExecuteQuery(queryFilterDto);
+
+        actual.Items.Should().BeEquivalentTo(expected);
+    }
 }
