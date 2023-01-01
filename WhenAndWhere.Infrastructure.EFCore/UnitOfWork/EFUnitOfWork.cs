1cf0c88116028d38b78cc2ecbc05203226ad27f2
Marek Kadlečík <xkadlec5@fi.muni.cz>
2023-01-01 21:29:46 +0100

    Removed transactions, delete option by its id

--- WhenAndWhere.Infrastructure.EFCore/UnitOfWork/EFUnitOfWork.cs
+++ WhenAndWhere.Infrastructure.EFCore/UnitOfWork/EFUnitOfWork.cs
@@ -8,39 +8,30 @@ namespace WhenAndWhere.Infrastructure.EFCore.UnitOfWork;
 public class EFUnitOfWork : IUnitOfWork
 {
     private WhenAndWhereDBContext _context;
-    private TransactionScope _scope;
 
     public WhenAndWhereDBContext Context => _context;
 
     public EFUnitOfWork(WhenAndWhereDBContext context)
     {
         _context = context;
-        _scope = new TransactionScope();
     }
 
     public void Dispose()
     {
-        _scope.Dispose();
         _context.Dispose();
     }
 
     public ValueTask DisposeAsync()
     {
-        _scope.Dispose();
         _context.DisposeAsync();
 
         return ValueTask.CompletedTask;
     }
 
-    public async Task RollbackAsync()
-    {
-        _scope = new TransactionScope();
-    }
+    public async Task RollbackAsync() { }
 
     public async Task CommitAsync()
     {
-        _scope.Complete();
         await _context.SaveChangesAsync();
-        _scope = new TransactionScope();
     }
 }
